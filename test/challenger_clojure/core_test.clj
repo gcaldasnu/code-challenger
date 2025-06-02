da5198ad925d897bcf449bf63791ab40997dd54b
Gedalias Caldas <gedalias.caldas@nubank.com.br>
2025-06-02 16:56:14 -0300

    feat: added filter and sum challenge (#1)
    * feat: added filter and sum challenge
    
    * refact: Improve tests

--- test/challenger_clojure/core_test.clj
+++ test/challenger_clojure/core_test.clj
@@ -11,5 +11,9 @@
       [1 2 3 + +] 6
       [1 2 "a"] nil)))
 
-(run-tests)
+(deftest filter-and-sum-test
+  (testing "Validate that only even values is sum"
+    (are [express expected-value] (= (filter-and-sum express) expected-value)
+      [1 2 3 4 5 6 7 8 9 10] 30
+      [1 3 4 6 7 3 3] 10)))
 
